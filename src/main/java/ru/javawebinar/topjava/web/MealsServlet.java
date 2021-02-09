7dae23555f891d0169c8e2393348624911ca7d49
vladyi <gumenskyi@gmail.com>
2021-02-09 09:31:59 +0200

    Add field meals to MealsServlet class

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -2,15 +2,18 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
 import java.util.List;
 
 import static ru.javawebinar.topjava.util.MealsUtil.CALORIES_PER_DAY;
@@ -18,16 +21,24 @@ import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
 
 public class MealsServlet extends HttpServlet {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MealsServlet.class);
-    private List<MealTo> meals;
+    private static final Logger log = LoggerFactory.getLogger(MealsServlet.class);
+    private List<MealTo> mealTos;
+    private List<Meal> meals = Arrays.asList(
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+    );
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (meals == null) {
-            meals = filteredByStreams(MealsUtil.init(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY);
-        }
+        log.debug("Get meal list");
+        mealTos = filteredByStreams(meals, LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY);
 
-        req.setAttribute("meals", meals);
+        req.setAttribute("meals", mealTos);
         req.getRequestDispatcher("meals.jsp").forward(req, resp);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -11,13 +11,12 @@ import java.io.IOException;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserServlet extends HttpServlet {
+
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to users");
-
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
 }
